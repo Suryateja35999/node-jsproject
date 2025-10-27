ed6b2e1c12b5e14b6aa0c078460bfe5686cb6602
Suryateja35999 <108362668+Suryateja35999@users.noreply.github.com>
2025-10-27 10:46:09 +0530

    Set up Express server with a welcome route

--- server.js
+++ server.js
@@ -1,1 +1,12 @@
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 8080;
+
+app.get("/", (req, res) => {
+  res.send("🚀 Node.js app deployed with Jenkins, Docker, and Minikube!");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
 
